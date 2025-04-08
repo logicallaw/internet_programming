ba410f09959ecd3fc80f7d5b548d8491959e9dfb
logicallaw <logicallawbio@gmail.com>
2025-04-08 20:39:29 +0900

    feat: add T06

---
+++ T06-EX/ex1.js
@@ -0,0 +1,10 @@
+var num1 = 3.456789;
+var num2 = 700000;
+var num3 = new Number('30.5');
+
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+console.log(Number.POSITIVE_INFINITY);
+
+console.log(num1.toFixed(2));
+console.log(num1.toPrecision(2));
